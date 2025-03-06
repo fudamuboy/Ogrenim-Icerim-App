8278a200cacd98454cf1f9a7f0a877fb71db55b2
samake <samakesal94@icloud.com>
2025-03-06 17:08:48 +0300

    updateoldu

--- components/CourseForm.js
+++ components/CourseForm.js
@@ -1,22 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './Input'
 
 export default function CourseForm() {
+    const [inputs, setInputs] = useState({
+        amount: '',
+        date: '',
+        description: ''
+    })
+    // console.log(inputs);
 
+    function inputChange(inputIdentifier, enteredValue) {
+        setInputs((currentInput) => {
+            return {
+                ...currentInput,
+                [inputIdentifier]: enteredValue
+            }
+        })
+    }
     return (
-        <View>
-            <Input label="Tutar"
-                textInputConfig={{
-                    keyboardType: 'decimal-pad',
-                    onChangeText: () => { }
-                }} />
-            <Input label="Tarih"
-                textInputConfig={{
-                    placeHolder: 'YYYY-MM-DD',
-                    maxLength: 10,
-                    onChangeText: () => { }
-                }} />
+        <View style={styles.form}>
+            <Text style={styles.basligi}>Kurs Bilgileri</Text>
+            <View style={styles.priceAndDate}>
+                <Input
+                    style={styles.flexAll}
+                    label="Tutar"
+                    textInputConfig={{
+                        keyboardType: 'decimal-pad',
+                        onChangeText: () => { inputChange('amount') },
+                        value: inputs.amount
+                    }} />
+                <Input label="Tarih"
+                    style={styles.flexAll}
+                    textInputConfig={{
+                        placeHolder: 'YYYY-MM-DD',
+                        maxLength: 10,
+                        onChangeText: () => { }
+                    }} />
+            </View>
+
             <Input label="Baslik bilgisi"
                 textInputConfig={{
                     multiline: true,
@@ -27,6 +49,24 @@ export default function CourseForm() {
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    priceAndDate: {
+        flexDirection: 'row',
+
+    },
+    flexAll: {
+        flex: 1,
+    },
+    form: {
+        marginTop: 40,
+    },
+    basligi: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'blue',
+        marginVertical: 20,
+    },
+})
 // CourseForm contient input qui possede ceux ci sont envoye ds ManageCourse
 // KeyboardType decimal fait de sorte que le clavier devient numerique 
--- components/Input.js
+++ components/Input.js
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
-export default function Input({ label, textInputConfig }) {
+export default function Input({ label, textInputConfig, style }) {
     const inputStyles = [styles.inputText] // op trop compris sonra bakcam
     if (textInputConfig && textInputConfig.multiline) {
         inputStyles.push(styles.inputMultiline)
     }
     return (
-        <View style={styles.inputContainer}>
+        <View style={[styles.inputContainer, style]}>
             <Text style={styles.label}>{label}</Text>
             <TextInput style={styles.inputStyles} {...textInputConfig} />
         </View>
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         marginHorizontal: 4,
         marginVertical: 10,
+
     },
     label: {
         fontSize: 15,
@@ -29,7 +30,7 @@ const styles = StyleSheet.create({
         padding: 5,
         borderRadius: 10,
         fontSize: 18,
-        paddingHorizontal: 5,
+
     },
     inputMultiline: {
         minHeight: 100,
